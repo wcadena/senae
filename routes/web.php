1d0f4a23418b2505bef05894aaccd04c8b3de7b5
Wágner Alexánder Cadena Lastra <wcadena@outlook.com>
2020-03-12 21:13:17 -0500

    descarga archivos rutas

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,19 @@ Route::get('/', function () {
 
 Route::get('formulario', 'StorageController@index');
 Route::post('storage/create', 'StorageController@save')->name('storage.create');
+Route::get('storage/{archivo}', function ($archivo) {
+    $public_path = public_path();
+    $url = $public_path.'/storage/'.$archivo;
+    //verificamos si el archivo existe y lo retornamos
+    if (\Illuminate\Support\Facades\Storage::exists($archivo))
+    {
+        return response()->download($url);
+    }
+    //si no se encuentra lanzamos un error 404.
+    abort(404);
+
+})->name('storage.index');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
